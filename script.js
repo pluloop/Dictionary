a28066688fe96e4c72f22beec275e3ff1ef2e21c
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-26 10:22:55 -0700

    pluloop first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var partOfSpeech = null;
+
+const definitions = {
+    yes:
+partOfSpeech = "none";
+definitions: 
+"Used to give an affirmative response.",
+"Used as a response to someone addressing one or otherwise trying to attract one's attention."
+panacea:
+part of speech: noun
+definitions: "An answer or solution for all problems or difficulties."
+concatenation:
+part of speech: noun
+definitions: A series of interconnected or interdependent things or events."
+saw:
+part of speech: verb
+definitions:
+"Cut (something) using a saw.",
+"Make rapid to-and-fro motions in cutting something or in playing a stringed instrument."
+found:
+part of speech: adjective
+definitions: 
+"Having been discovered by chance or unexpectedly.",
+"(of a ship) Equipped; Supplied."
+crane:
+part of speech: verb
+definitions: 
+"Stretch out one's body or neck in order to see something.",
+"Move (a heavy object) with a crane."
+minute:
+part of speech: noun
+definitions: 
+"A period of time equal to sixty seconds or a sixtieth of an hour.",
+"A sixtieth of a degree of angular measurement (symbol: ʹ)"
+grotesque:
+part of speech:adjective
+definitions: "Comically or repulsively ugly or distorted."
+label:
+part of speech: noun
+definitions: "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it."
+debacle:
+part of speech: noun
+definitions: "A sudden and ignominious failure; a fiasco."
+}
