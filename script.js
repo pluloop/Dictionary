2a8739af35fdc707f32feb6972b84ae1d53a85f8
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-10-27 10:30:30 -0700

    plu added text validation

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
-var partOfSpeech = null;
+var result = document.getElementById("result")
+var textInput = document.getElementById("text-input");
+const submitButton = document.getElementById("submit-button");
+var partOfSpeech;
+var definition1;
+var definition2;
 
 const definitions = {
-    yes:
-partOfSpeech = "none";
-definitions: 
-"Used to give an affirmative response.",
-"Used as a response to someone addressing one or otherwise trying to attract one's attention."
-panacea:
-part of speech: noun
-definitions: "An answer or solution for all problems or difficulties."
-concatenation:
-part of speech: noun
-definitions: A series of interconnected or interdependent things or events."
-saw:
-part of speech: verb
-definitions:
-"Cut (something) using a saw.",
-"Make rapid to-and-fro motions in cutting something or in playing a stringed instrument."
-found:
-part of speech: adjective
-definitions: 
-"Having been discovered by chance or unexpectedly.",
-"(of a ship) Equipped; Supplied."
-crane:
-part of speech: verb
-definitions: 
-"Stretch out one's body or neck in order to see something.",
-"Move (a heavy object) with a crane."
-minute:
-part of speech: noun
-definitions: 
-"A period of time equal to sixty seconds or a sixtieth of an hour.",
-"A sixtieth of a degree of angular measurement (symbol: ʹ)"
-grotesque:
-part of speech:adjective
-definitions: "Comically or repulsively ugly or distorted."
-label:
-part of speech: noun
-definitions: "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it."
-debacle:
-part of speech: noun
-definitions: "A sudden and ignominious failure; a fiasco."
+    yes: {
+        partOfSpeech: "noun", 
+        definition1: "Used to give an affirmative response.", 
+        definition2: "Used as a response to someone addressing one or otherwise trying to attract one's attention."
+    },
+
+    panacea: {
+        partOfSpeech: "noun",
+        definition1: "An answer or solution for all problems or difficulties."
+    },
+
+    concatenation: {
+        partOfSpeech: "noun",
+        definition1: "A series of interconnected or interdependent things or events."
+    },
+
+    saw: {
+        partOfSpeech: "verb",
+        definition1: "Cut (something) using a saw.",
+        definition2: "Make rapid to-and-fro motions in cutting something or in playing a stringed instrument."
+    },
+
+    found: {
+        partOfSpeech: "adjective",
+        definition1: "Having been discovered by chance or unexpectedly.",
+        definition2: "(of a ship) Equipped; Supplied."
+    },
+
+    crane: {
+        partOfSpeech: "verb",
+        definition1: "Stretch out one's body or neck in order to see something.",
+        definition2: "Move (a heavy object) with a crane."
+    },
+
+    minute: {
+        partOfSpeech: "noun",
+        definition1: "A period of time equal to sixty seconds or a sixtieth of an hour.",
+        definition2: "A sixtieth of a degree of angular measurement (symbol: ʹ)"
+    },
+
+    grotesque: {
+        partOfSpeech: "adjective",
+        definition1: "Comically or repulsively ugly or distorted."
+    },
+
+    label: {
+        partOfSpeech: "noun",
+        definition1: "A small piece of paper, fabric, plastic, or similar material attached to an object and giving information about it."
+    },
+
+    debacle: {
+        partOfSpeech: "noun",
+        definition1: "A sudden and ignominious failure; a fiasco."
+    }
 }
+
+let errorMessage = document.createElement("p");
+errorMessage.classList.add("error-message");
+errorMessage.innerText = "Error: you need to input a word.";
+
+function checkInput(){
+    if (textInput.value == ""){
+        result.appendChild(errorMessage);
+    }
+
+    else{
+        result.removeChild(errorMessage);
+    }
+
+    textInput.value = "";
+}
+
+submitButton.addEventListener("click", checkInput);
